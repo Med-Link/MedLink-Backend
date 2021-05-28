fc0337dfe2520a9d6e3f048e9297b508c998bc48
Akila De Silva <akiladesilva97@gmail.com>
2021-05-29 01:43:33 +0530

    database connected

--- src/index.server.js
+++ src/index.server.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const env = require("dotenv");
 const app = express();
+const mongoose = require('mongoose');
+
+
+//routes
+const userRoutes = require('./routes/user');
+
+env.config();
+
+//mongodb+srv://root:<password>@cluster0.icjfy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+mongoose.connect(
+      `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.icjfy.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`, 
+      {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true
+      }
+).then(()=>{
+      console.log('Database connected');
+});
 
 app.listen(process.env.PORT, ()=>{
-      console.log('SERVER is running on port ${process.env.PORT');
+      console.log(`Server is running on port ${process.env.PORT}`);
 });
