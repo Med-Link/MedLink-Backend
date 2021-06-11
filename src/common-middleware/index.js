febf6d0c13a705a89533ac9aba955451bc67e9f4
Akila De Silva <akiladesilva97@gmail.com>
2021-06-12 02:02:22 +0530

    Authorization for user roles

--- src/common-middleware/index.js
+++ src/common-middleware/index.js
@@ -1,8 +1,31 @@
 exports.requireSignin = (req, res, next) => {
-      const token = req.headers.authorization.split(" ")[1];
-      const user = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = user;
-      next();
-      // jwt.decode()
-    };
-    
+  if (req.headers.authorization) {
+    const token = req.headers.authorization.split(" ")[1];
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  }
+  return res.status(400).json({ message: "Authorization required" });
+
+  // jwt.decode()
+};
+exports.adminMiddleware = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(400).json({ message: "Access Denied" });
+  }
+  next();
+};
+
+exports.userMiddleware = (req, res, next) => {
+  if (req.user.role !== "user") {
+    return res.status(400).json({ message: "Access Denied" });
+  }
+  next();
+};
+
+exports.pharmacyMiddleware = (req, res, next) => {
+  if (req.user.role !== "pharmacy") {
+    return res.status(400).json({ message: "Access Denied" });
+  }
+  next();
+};
