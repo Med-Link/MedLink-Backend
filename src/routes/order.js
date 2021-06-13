0d1bfae3c65cfd540b2d6c2d3af2b73dbe2665b6
Akila De Silva <akiladesilva97@gmail.com>
2021-06-14 00:23:40 +0530

    s3 bucket set up

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { addOrder, getOrders } = require("../controller/order");
 const { requireSignin, userMiddleware } = require("../common-middleware");
+const multer=require('multer');
+const upload=multer({dest:''});
 const router = express.Router();
 
-router.post("/order/create", requireSignin, userMiddleware, addOrder);
+router.post("/order/create", requireSignin, userMiddleware, upload.single('prescription'), addOrder);
 router.get("/order/getOrder", getOrders);
 
 module.exports = router;
---
+++ src/s3.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const S3=require('aws-sdk/client/s3');
+
+
+const bucketname =process.env.AWS_BUCKET_NAM
+const region=process.env.AWS_BUCKET_REGION
+const accessKeyId=process.env.AWS_ACCESS_KEY
+const secretAccessKey=process.env.AWS_SECRET_KEY
+
+const s3=new S3({
+      region,
+      accessKeyId,
+      secretAccessKey
+
+})
+
+//upload a file
+
+
+
+//download a file
