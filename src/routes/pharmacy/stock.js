8ebf210eadc9ed5fc5e2d22234ce9903cbfdf527
MichelleFdo <mufernando02@gmail.com>
2021-09-10 03:49:34 +0530

    medicine listdown for add new medicine batch

--- src/routes/pharmacy/stock.js
+++ src/routes/pharmacy/stock.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { requireSignin, pharmacyMiddleware } = require('../../common-middleware');
 const {
-  addstock, updatestock, deletestock, viewallstock, viewsinglestock,
+  addstock, updatestock, deletestock, viewallstock, viewsinglestock, listmedicine
 } = require('../../controller/pharmacy/stock');
 
 const router = express.Router();
@@ -11,5 +11,6 @@ router.post('/pharmacy/updatestock', requireSignin, pharmacyMiddleware, updatest
 router.delete('/pharmacy/deletestock', requireSignin, pharmacyMiddleware, deletestock);
 router.get('/pharmacy/viewallstock', requireSignin, pharmacyMiddleware, viewallstock);
 router.post('/pharmacy/viewsinglestock', requireSignin, pharmacyMiddleware, viewsinglestock);
+router.get('/pharmacy/listmedicine', requireSignin, pharmacyMiddleware, listmedicine);
 
 module.exports = router;
