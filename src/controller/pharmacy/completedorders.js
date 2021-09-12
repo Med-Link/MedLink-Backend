55a42a40307cc2dc9e4938d9799a75f34ef4cbf7
MichelleFdo <mufernando02@gmail.com>
2021-09-12 23:59:35 +0530

    complete order count query

--- src/controller/pharmacy/completedorders.js
+++ src/controller/pharmacy/completedorders.js
@@ -26,4 +26,28 @@ exports.viewallcloseddeals = async (req, res) => {
       res.status(500).send('Server error');
     }
   };
+
+  exports.countcloseddeals = async (req, res) => {
+    const token = req.headers.authorization.split(' ')[1];
+  
+    const decoded = jwt.decode(token, process.env.JWT_SECRET);
+    const pharmacyid = decoded.payload.id;
+  
+    try {
+      const alldeals = await pool.query('SELECT count(orderid) as completeOrderCount FROM public.completedorder, public.order_medlist, public.customers WHERE completedorder.orderid=order_medlist.order_reqid AND order_medlist.customerid=customers.customerid AND pharmacyid = $1', 
+      [pharmacyid]);
+  
+      const result = alldeals.rows;
+  
+      if (alldeals) {
+        return res.status(200).json({
+          message: 'all cloased deals listed success',
+          result,
+        });
+      }
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  };  
   
