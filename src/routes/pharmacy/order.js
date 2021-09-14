667936fbc0856db350200d3d596150db6415b5d1
Akila De Silva <akiladesilva97@gmail.com>
2021-09-14 22:25:19 +0530

    pharmacy order request reject

--- src/routes/pharmacy/order.js
+++ src/routes/pharmacy/order.js
@@ -1,11 +1,13 @@
 const express = require('express');
 // eslint-disable-next-line camelcase
-const { getPharmacyOrder_reqs, getPharmacyOrder_req } = require('../../controller/pharmacy/order');
+const { getPharmacyOrder_reqs, getPharmacyOrder_req, rejectOrder_req } = require('../../controller/pharmacy/order');
 const { requireSignin, pharmacyMiddleware } = require('../../common-middleware');
 
 const router = express.Router();
 
 router.get('/pharmacy/getOrderReqs', requireSignin, pharmacyMiddleware, getPharmacyOrder_reqs);
 router.post('/pharmacy/getOrderReq', requireSignin, pharmacyMiddleware, getPharmacyOrder_req);
+router.post('/pharmacy/rejectOrderReq', requireSignin, pharmacyMiddleware, rejectOrder_req);
+
 
 module.exports = router;
