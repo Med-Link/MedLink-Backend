74fcc844f85be81e616a1ef5c6b44fe731555c35
Akila De Silva <akiladesilva97@gmail.com>
2021-09-16 01:54:19 +0530

    pharmacy closed deals

--- src/routes/pharmacy/completedorders.js
+++ src/routes/pharmacy/completedorders.js
@@ -2,12 +2,14 @@ const express = require('express');
 // // eslint-disable-next-line camelcase
 // const { getPharmacyOrder_reqs, getPharmacyOrder_req } = require('../../controller/pharmacy/order');
 const { requireSignin, pharmacyMiddleware } = require('../../common-middleware');
-const { viewallcloseddeals, countcloseddeals } = require('../../controller/pharmacy/completedorders');
+const { countcloseddeals, closeddeals } = require('../../controller/pharmacy/completedorders');
 
 
 const router = express.Router();
 
-router.get('/pharmacy/viewallcloseddeals', requireSignin, pharmacyMiddleware, viewallcloseddeals);
+// router.get('/pharmacy/viewallcloseddeals', requireSignin, pharmacyMiddleware, viewallcloseddeals);
 router.get('/pharmacy/countcloseddeals', requireSignin, pharmacyMiddleware, countcloseddeals);
+router.get('/pharmacy/closeddeals', requireSignin, pharmacyMiddleware, closeddeals);
+
 
 module.exports = router;
