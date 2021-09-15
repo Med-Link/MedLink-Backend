3967a59ccce2d8bbcedb953c87713def6c97ae12
Akila De Silva <akiladesilva97@gmail.com>
2021-09-15 21:51:10 +0530

    pharmacy auth routes

--- src/routes/pharmacy/auth.js
+++ src/routes/pharmacy/auth.js
@@ -2,12 +2,15 @@ const express = require('express');
 const { uploadpS3, requireSignin } = require('../../common-middleware');
 
 const router = express.Router();
-const { signup, signin, signout } = require('../../controller/pharmacy/auth');
+const { signup, signin, signout, forgotpassword, resetpassword, verifyemail } = require('../../controller/pharmacy/auth');
 const { isRequestValidated, validateSigninRequest, validateSignupRequestpharm } = require('../../validators/auth');
 
 router.post('/pharmacy/signin', validateSigninRequest, isRequestValidated, signin);
 router.post('/pharmacy/signup', uploadpS3.array('registrationDocs'), validateSignupRequestpharm, isRequestValidated, signup);
 router.post('/pharmacy/signout', signout);
+router.post('/pharmacy/forgotpassword', forgotpassword);
+router.post('/pharmacy/resetpassword', resetpassword);
+router.post('/verifyemail', verifyemail);
 
 // router.post('/profile', requireSignin,(req,res)=>{
 //       res.status(200).json({ user:'profile'})
