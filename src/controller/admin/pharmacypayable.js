ea33603a64f3d85a2bfe3bedfb03c808a67a2f5b
Akila De Silva <akiladesilva97@gmail.com>
2021-09-18 00:04:47 +0530

    list pharmacy payables

---
+++ src/controller/admin/pharmacypayable.js
@@ -0,0 +1,42 @@
+const pool = require('../../db/db');
+
+exports.viewpayablepharmacy = async (req, res) => {
+  const requests = await pool.query('SELECT pharmacyid, SUM(medlisttotal) FROM completedorder WHERE pharmacypaid = $1 AND paymentstatus = $2 GROUP BY pharmacyid', [
+    true, true,
+  ]);
+  if (requests.rows.length === 0) {
+    return res.status(400).json('server error');
+  }
+  const result = requests.rows;
+  if (requests) {
+    return res.status(200).json({
+      message: 'Pharmacy signup request listed success',
+      result,
+    });
+  }
+  return res.status(400).json({ error: 'Server error' });
+};
+
+exports.pharmacypaid = async (req, res) => {
+  const {
+    pharmacyid,
+  } = req.body;
+
+  try {
+    const pharmacypaid = await pool.query(
+      'UPDATE public.completedorder SET pharmacypaid = $1 WHERE pharmacyid = $2', [
+        true, pharmacyid,
+      ],
+    );
+    if (pharmacypaid) {
+      return res.status(200).json({
+        message: 'order bill accepted success',
+        // rows,
+
+      });
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
