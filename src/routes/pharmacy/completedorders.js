86c9a344c0a5e3534fe8dcf64a9daef6b7e63005
Akila De Silva <akiladesilva97@gmail.com>
2021-09-19 20:57:41 +0530

    pharmacy shipped order routes

--- src/routes/pharmacy/completedorders.js
+++ src/routes/pharmacy/completedorders.js
@@ -2,7 +2,7 @@ const express = require('express');
 // // eslint-disable-next-line camelcase
 // const { getPharmacyOrder_reqs, getPharmacyOrder_req } = require('../../controller/pharmacy/order');
 const { requireSignin, pharmacyMiddleware } = require('../../common-middleware');
-const { countcloseddeals, closeddeals } = require('../../controller/pharmacy/completedorders');
+const { countcloseddeals, closeddeals, shippeddeals, markshipped } = require('../../controller/pharmacy/completedorders');
 
 
 const router = express.Router();
@@ -10,6 +10,10 @@ const router = express.Router();
 // router.get('/pharmacy/viewallcloseddeals', requireSignin, pharmacyMiddleware, viewallcloseddeals);
 router.get('/pharmacy/countcloseddeals', requireSignin, pharmacyMiddleware, countcloseddeals);
 router.get('/pharmacy/closeddeals', requireSignin, pharmacyMiddleware, closeddeals);
+router.get('/pharmacy/shippeddeals', requireSignin, pharmacyMiddleware, shippeddeals);
+router.post('/pharmacy/shippeddeals', requireSignin, pharmacyMiddleware, markshipped);
+
+
 
 
 module.exports = router;
