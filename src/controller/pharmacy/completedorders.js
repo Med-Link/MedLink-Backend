99c2eed51e161abb7f8cfb8904654f9ebee33b7a
Akila De Silva <akiladesilva97@gmail.com>
2021-09-19 20:40:11 +0530

    pharmacy order mark as shipped view shipped orders

--- src/controller/pharmacy/completedorders.js
+++ src/controller/pharmacy/completedorders.js
@@ -33,14 +33,60 @@ exports.closeddeals = async (req, res) => {
 
   try {
     const getorderhistory = await pool.query(
-      'SELECT * FROM public.completedorder INNER JOIN public.customers ON completedorder.customerid = customers.customerid WHERE pharmacyid = $1 AND paymentstatus = $2', [
-        pharmacyid, true,
+      'SELECT * FROM public.completedorder INNER JOIN public.customers ON completedorder.customerid = customers.customerid WHERE pharmacyid = $1 AND paymentstatus = $2 shipped =$3', [
+        pharmacyid, true, false,
       ],
     );
     if (getorderhistory) {
       return res.status(200).json({
         message: 'order history listed',
         getorderhistory,
+      });
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
+
+exports.markshipped = async (req, res) => {
+  const {
+    orderid,
+  } = req.body;
+
+  try {
+    const marked = await pool.query(
+      'UPDATE public.completed order SET shipped = $1 WHERE orderid = $2', [
+        true, orderid,
+      ],
+    );
+    if (marked) {
+      return res.status(200).json({
+        message: 'orders marked as shipped rejected ',
+      });
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
+
+exports.shippeddeals = async (req, res) => {
+  const token = req.headers.authorization.split(' ')[1];
+
+  const decoded = jwt.decode(token, process.env.JWT_SECRET);
+  const pharmacyid = decoded.payload.id;
+
+  try {
+    const getshippedorders = await pool.query(
+      'SELECT * FROM public.completedorder INNER JOIN public.customers ON completedorder.customerid = customers.customerid WHERE pharmacyid = $1 AND paymentstatus = $2 AND shipped =$3', [
+        pharmacyid, true, true,
+      ],
+    );
+    if (getshippedorders) {
+      return res.status(200).json({
+        message: 'order history listed',
+        getshippedorders,
       });
     }
   } catch (err) {
