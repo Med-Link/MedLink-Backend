ea23992cbb4b319fa09f44c9a91d7fdc13d921df
MichelleFdo <mufernando02@gmail.com>
2021-09-20 22:48:47 +0530

    dashboard -max sales pharmacy, average transactions per day

--- src/routes/admin/dashboard.js
+++ src/routes/admin/dashboard.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const { requireSignin, adminMiddleware } = require('../../common-middleware');
-const { countcustomer, countpharmacy,viewmonthlyincome } = require('../../controller/admin/dashboard');
+const { countcustomer, countpharmacy,maxsalespharmacy,averageorderspermonth} = require('../../controller/admin/dashboard');
 
 const router = express.Router();
-// const { isRequestValidated, validateSigninRequest } = require('../../validators/auth');
-
-// router.post('/admin/addnewAdmin', validateSigninRequest, isRequestValidated, addnewAdmin);
 router.get('/admin/countcustomer', requireSignin, adminMiddleware, countcustomer);
 router.get('/admin/countpharmacy', requireSignin, adminMiddleware, countpharmacy);
+router.get('/admin/maxsalespharmacy', requireSignin, adminMiddleware, maxsalespharmacy);
+router.get('/admin/averageorderspermonth', requireSignin, adminMiddleware, averageorderspermonth);
 
 module.exports = router;
