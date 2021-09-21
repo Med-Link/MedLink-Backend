16bcf9d2c391850275cba942c0ece1a44c5ff938
MichelleFdo <mufernando02@gmail.com>
2021-09-22 04:13:57 +0530

    view income report by custom time range

--- src/controller/admin/income.js
+++ src/controller/admin/income.js
@@ -39,3 +39,28 @@ exports.totalmonthlyincome = async (req, res) => {
   return res.status(400).json({ error: 'Server error' });
 };
 
+exports.customedaterangeincome = async (req, res) => {
+  const {
+    fromdate, todate,
+  } = req.body;
+  try {
+    const customincome = await pool.query(
+      ` SELECT completedorder.pharmacyid, SUM(completedorder.servicecost),pharmacy.name 
+        FROM completedorder 
+        INNER JOIN pharmacy ON completedorder.pharmacyid=pharmacy.pharmacyid 
+        WHERE paymentstatus = true AND completedorder.date >= $1 AND completedorder.date<=$2
+        GROUP BY completedorder.pharmacyid,pharmacy.name`,
+      [fromdate, todate],
+    );
+    const result = customincome.rows;
+    if (customincome) {
+      return res.status(201).json({
+        message: 'Income 555 Listed Successfully',
+        result,
+      });
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
--- src/routes/admin/income.js
+++ src/routes/admin/income.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 const { requireSignin, adminMiddleware } = require('../../common-middleware');
-const { viewmonthlyincome,totalmonthlyincome } = require('../../controller/admin/income');
+const { viewmonthlyincome,totalmonthlyincome,customedaterangeincome } = require('../../controller/admin/income');
 
 router.get('/admin/viewmonthlyincome', requireSignin, adminMiddleware, viewmonthlyincome);
 router.get('/admin/totalmonthlyincome', requireSignin, adminMiddleware, totalmonthlyincome);
+router.post('/admin/customedaterangeincome', requireSignin, adminMiddleware, customedaterangeincome);
+
 
 module.exports = router;
