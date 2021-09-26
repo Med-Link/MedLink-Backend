fdc3a7fa8f1f846cb9999caea8b48306c8a65e27
Akila De Silva <akiladesilva97@gmail.com>
2021-09-26 17:41:17 +0530

    set header

--- src/index.server.js
+++ src/index.server.js
@@ -44,6 +44,9 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
   next();
 });
 
