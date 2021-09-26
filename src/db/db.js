533f1136b3ed3d964f0527c5e4627e518f8242ab
Akila De Silva <akiladesilva97@gmail.com>
2021-09-26 21:49:37 +0530

    database host changing

--- src/db/db.js
+++ src/db/db.js
@@ -13,7 +13,7 @@ const knexfile = require('./knexfile');
 const pool = new Pool({
   user: 'aeeurajvblzrqz',
   password: '186bdc04eead08fda182f5eafb5306df3112b2e2ac8388cd3afac47eb0d2ebbd',
-  host: 'localhost',
+  host: 'ec2-44-195-201-3.compute-1.amazonaws.com',
   port: 5432,
   database: 'dk5206v3k9k8o',
 
